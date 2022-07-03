e8a3146a32b1758515c547601dfc81095df90dd2
Aytekin KALMA <100280160+aytekinkalma@users.noreply.github.com>
2022-07-03 14:35:40 +0000

    Add files via upload

---
+++ Statistics/assigment2.py
@@ -0,0 +1,55 @@
+STATISTICS
+"""
+ASSIGNMENT (CONFIDENCE INTERVALS) 
+
+EXERCISE 1. 
+Suppose scores on exams in statistics are normally distributed with an unknown
+ population mean and a population standard deviation of 3 points.
+ A random sample of 36 scores is taken and gives a sample mean
+ (sample  mean score) of 68. Find a confidence interval estimate 
+ for the population mean exam score (the mean score on all exams).
+Find a 90% confidence interval for the true (population) mean of statistics exam scores. 
+
+ Confidence Interval = (x̄ – z * ơ / √n) to(x̄ + z * ơ / √n)
+
+
+n = 36  confidence interval=(68-(1/2)(1.6449), 68+(1/2)(1.6449)= ( 67.18, 68.82)
+
+
+
+
+EXERCISE 2. 
+What is the normal body temperature for healthy humans? 
+A random sample of 130 healthy human body temperatures 
+provided by Allen Shoemaker7 yielded 98.25 degrees and standard deviation 0.73 degrees. 
+Give a 99% confidence interval for the average body temperature of healthy people.
+
+ n=130
+ x̄=98.25
+ ơ=0.73
+ a=0.01  (2.576)
+ Confidence Interval = (x̄ – z * ơ / √n) to(x̄ + z * ơ / √n)
+ 
+ =(98.25-2.576*0.73/√130) to (98.25+2.576*0.73/√130)
+ =(98.086,98.414)
+
+
+
+
+EXERCISE 3. 
+The administrators for a hospital wished to estimate the average number of
+ days required for inpatient treatment of patients between the ages of 25 
+ and 34. A random sample of 500 hospital patients between these ages 
+ produced a mean and standard deviation equal to 5.4 and 3.1 days, respectively.
+Construct a 95% confidence interval for the mean length of stay 
+for the population of patients from which the sample was drawn.
+ 
+ n=500
+ x̄=5.4
+ ơ=3.1
+ a=0.05  (1.96)
+
+Confidence Interval = (x̄ – z * ơ / √n) to(x̄ + z * ơ / √n)
+
+=(5.14-1.96*3.1/√500) to (5.14+1.96*3.1/√500) =(5.1,5.7)  
+"""
